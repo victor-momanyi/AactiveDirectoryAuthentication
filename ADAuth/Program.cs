11bf5e663cea4fb2dd14dce06827aeda54228eb8
vmomanyi <mosoti.victor@gmail.com>
2025-12-18 15:31:12 +0300

    Removed comments from Program.cs

--- ADAuth/Program.cs
+++ ADAuth/Program.cs
@@ -25,8 +25,6 @@ builder.Services.AddAuthorization();
 builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
